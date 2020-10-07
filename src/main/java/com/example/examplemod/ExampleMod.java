3365b1b769cd7870551750c0985ce6bcc9f8ddb2
Dmitry Demin <rayzer656@gmail.com>
2020-10-07 12:46:55 +0300

    Import of a forge standart mod structure

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,40 @@
+package com.example.examplemod;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("more_totems")
+public class ExampleMod
+{
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "more_totems";
+
+    public ExampleMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+
+    public static final ItemGroup TAB = new ItemGroup("MoreTotemsTab") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(Items.TOTEM_OF_UNDYING.getItem());
+        }
+    };
+
+}
